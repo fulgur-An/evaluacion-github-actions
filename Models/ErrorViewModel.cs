80631198254bf7c5d388aa0d2c430c4aaa16ec4a
DOMINGUEZ GARCIA ANTONIO DE JESUS <zS19014049@estudiantes.uv.mx>
2026-03-13 12:00:53 -0600

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace evaluacion_github_actions.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
